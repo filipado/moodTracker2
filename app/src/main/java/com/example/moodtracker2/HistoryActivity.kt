50facc4480fcdccea31fd514ff18c419ea7c4361
Filip Grzejszczyk <f.grzejszczyk@gmail.com>
2020-11-15 22:15:09 +0000

    History Activity newest

--- app/src/main/java/com/example/moodtracker2/HistoryActivity.kt
+++ app/src/main/java/com/example/moodtracker2/HistoryActivity.kt
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import kotlinx.android.synthetic.main.activity_history.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Comment
+import java.security.cert.LDAPCertStoreParameters
+import java.text.FieldPosition
 import java.time.LocalDate
 
 class HistoryActivity : AppCompatActivity() {
@@ -17,23 +23,88 @@ class HistoryActivity : AppCompatActivity() {
         //shared preferences to access mood
         val sharedPref = getSharedPreferences("mood", MODE_PRIVATE)
         val date = LocalDate.now().toString()
-
         val savedMood = sharedPref.getInt(date, 0)
 
-        yesterday_textView.setText(savedMood.toString())
+        recyclerview_id.setOnScrollChangeListener() {
 
-        //shared preferences to access comment
-        val sharedPrefComment = getSharedPreferences("comment", MODE_PRIVATE)
-        val savedComment = sharedPrefComment.getString(date, null)
+            //shared preferences to access mood
+            val sharedPref = getSharedPreferences("mood", MODE_PRIVATE)
+            val date = LocalDate.now().toString()
+            val savedMood = sharedPref.getInt(date, 0)
 
 
-        //alertDialog to show comment:
-        comment_imageButton.setOnClickListener {
-            val builder = AlertDialog.Builder(this)
-            builder.setMessage(savedComment.toString())
-            builder.show()
+            if (savedMood == 0) {
+                yesterday_constraintLayout.setBackgroundColor(resources.getColor(R.color.banana_yellow))
+            }
+            if (savedMood == 1) {
+                yesterday_constraintLayout.setBackgroundColor(resources.getColor(R.color.light_sage))
+            }
         }
 
+        //moods class
+        fun Mood(){
+            val sharedPref = getSharedPreferences("mood", MODE_PRIVATE)
+            val date = LocalDate.now().toString()
+            val savedMood = sharedPref.getInt(date, 0)
 
+            var yesterdayConsLay = findViewById<ConstraintLayout>(R.id.yesterday_constraintLayout)
+            var twoDayAgoConsLay =
+                findViewById<ConstraintLayout>(R.id.two_days_ago_constraintLayout)
+            var threeDaysAgoConsLay =
+                findViewById<ConstraintLayout>(R.id.three_days_ago_constraintLayout)
+            var fourDaysAgoConsLay =
+                findViewById<ConstraintLayout>(R.id.four_days_ago_constraintLayout)
+            var fiveDaysAgoConsLay =
+                findViewById<ConstraintLayout>(R.id.five_days_ago_constraintLayout)
+            var sixDaysAgoConsLay =
+                findViewById<ConstraintLayout>(R.id.six_days_ago_constraintLayout)
+            var sevenDaysAgoConsLay =
+                findViewById<ConstraintLayout>(R.id.one_week_ago_constraintLayout)
+
+            val today = LocalDate.now()
+            val yesterday = LocalDate.now().minusDays(1)
+            val twoDaysAgo = LocalDate.now().minusDays(2)
+            val threeDaysAgo = LocalDate.now().minusDays(3)
+            val fourDaysAgo = LocalDate.now().minusDays(4)
+            val fiveDaysAgo = LocalDate.now().minusDays(5)
+            val sixDaysAgo = LocalDate.now().minusDays(6)
+            val sevenDAysAgo = LocalDate.now().minusWeeks(1)
+
+
+/*
+            when (savedMood) {
+                0 -> {
+                    setWidthPercent(1)
+                    setBackgroundColor(R.color.banana_yellow)
+                }
+
+                1 -> {
+                    setWidthPercent(0.8)
+                    setBackgroundColor(R.color.light_sage)
+                }
+
+                2 -> {
+                    setWidthPercent(0.6)
+                    setBackgroundColor(R.color.cornflower_blue_65)
+                }
+
+                3 -> {
+                    setWidthPercent(0.4)
+                    setBackgroundColor(R.color.warm_grey)
+                }
+
+                4 -> {
+                    setWidthPercent(0.2)
+                    setBackgroundColor(R.color.faded_red)
+                }
+            }
+
+ */
+
+            //shared preferences to access comment
+            val sharedPrefComment = getSharedPreferences("comment", MODE_PRIVATE)
+            val savedComment = sharedPrefComment.getString(date, null)
+
+        }
     }
 }
--- app/src/main/java/com/example/moodtracker2/adapter.kt
+++ app/src/main/java/com/example/moodtracker2/adapter.kt
@@ -1,20 +1,19 @@
 package com.example.moodtracker2
 
-import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.mood_layout.view.*
-import java.time.LocalDate
 import java.time.LocalDateTime
 
+//initiating ViewHolder
 class MyViewHolder(val view : View):RecyclerView.ViewHolder(view){}
 
+//initiating Adapter
 class MyAdapter : RecyclerView.Adapter<MyViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -27,17 +26,20 @@ class MyAdapter : RecyclerView.Adapter<MyViewHolder>(){
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
+        val context = holder.view.context
+
+        //Adding Shared Preferences to be able to access the position from other activities
         val sharedPref = holder.view.context.getSharedPreferences("mood", MODE_PRIVATE)
         val editor = sharedPref.edit()
 
-        //Get the current date
+        //Get the current date to be able to choose the mood for the day and be able to add it to history activity
         val date = LocalDateTime.now().toString()
 
+        //Extracting position through shared preferences
         editor.putInt(date, position)
         editor.apply()
 
-        val context = holder.view.context
-
+        //Adding values and "when" method to initiate recycler view data
         val emoji = holder.view.emoji_img
         val background = holder.view.moodLayout_id
         val moodSelected = position
@@ -45,7 +47,6 @@ class MyAdapter : RecyclerView.Adapter<MyViewHolder>(){
         when(moodSelected){
             0 -> {emoji.setImageResource(R.drawable.smiley_super_happy)
                 background.setBackgroundColor(context.resources.getColor(R.color.banana_yellow))
-
             }
 
             1 -> {emoji.setImageResource(R.drawable.smiley_happy)
